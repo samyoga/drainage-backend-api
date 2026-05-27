c7a95e6033f6670a93e7fb18a0462a1f220f821f
Samyoga Bhattarai <sbhatt26@charlotte.edu>
2026-05-27 16:36:22 -0400

    Create simple model for API

--- inlets/models.py
+++ inlets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class InletPoint(models.Model):
+    pointid = models.IntegerField()
+    name = models.CharField(max_length = 255, blank = True)
+    status = models.CharField(max_length = 255, blank = True)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add = True)
